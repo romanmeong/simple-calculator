3e63f2786eef6c95965e4d300f5bbb7bf277d903
Tengku Romansyah <tengku.romansyah@gmail.com>
2019-11-27 18:42:21 +0700

    Add and delete function added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 M.AutoInit();
+
+function AddText(value) {
+    var currentValue = document.getElementById("calcscreen").value;
+    document.getElementById("calcscreen").value = currentValue + value;
+}
+
+function DeleteText(clearOrDel) {
+    if (clearOrDel) {
+        document.getElementById("calcscreen").value = "";
+    }
+    else {
+        var currentValue = document.getElementById("calcscreen").value;
+        var value = currentValue.substring(0, currentValue.length-1);
+        document.getElementById("calcscreen").value = value;
+    } 
+}
